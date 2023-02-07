55f3c8de3a29171e72154611ff6e054e082c8536
Heidi Fryzell <hfryzell@gmail.com>
2023-02-07 06:46:29 -0500

    inital setup

--- Dog.js
+++ Dog.js
@@ -1,1 +1,10 @@
 // Create the Dog class here
+import { dogs } from "./data"
+
+class Dog {
+    constuctor(data){
+
+    }
+}
+
+export {Dog}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Remember to import the data and Dog class!
+
+import { dogs } from "./data";
+// import { Dog } from "./Dog";
+
+alert('hello')
+console.log('test')
