e727257842e8e08ffb17e9b68c9fe3d01fb37d48
Heidi Fryzell <hfryzell@gmail.com>
2023-02-08 08:57:52 -0500

    rendering first dog and buttons working

--- Dog.js
+++ Dog.js
@@ -6,15 +6,33 @@ class Dog {
     }
     getDogHtml(){
         return `
-        <div>
-        <div>${this.name}</div>
-        <div>${this.avatar}</div>
-        <div>${this.age}</div>
-        <div>${this.bio}</div>
-        <div>${this.hasBeenSwiped}</div>
-        <div>${this.hasBeenLiked}</div>
+        <div class="profile-img">
+          <img src="${this.avatar}" alt="${this.name}" />
+        </div>
+        <div class="profile-text">
+          <h1>${this.name}, ${this.age}</h1>
+          <p>${this.bio}</p>
         </div>
         `
+    }
+    getLikeHtml(){
+        if(!this.hasBeenSwiped){
+            this.hasBeenSwiped = true;
+            this.hasBeenLiked = true;
+            return `
+            <div class="profile-like">
+            <img src="images/badge-like.png" alt="like badge" />
+            </div>`
+        }
+    }
+    getNopeHtml(){
+        if(!this.hasBeenSwiped){
+            this.hasBeenSwiped = true;
+        return `
+        <div class="profile-like">
+        <img src="images/badge-nope.png" alt="nope badge" />
+        </div>`
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -4,9 +4,36 @@ import dogs from './data.js'
 import Dog from "./Dog.js"
 
 
-const rex = new Dog(dogs[0])
-const bella = new Dog(dogs[1])
-const teddy = new Dog(dogs[2])
+let dogProfile = new Dog(dogs[0])
 
-const dogDiv = document.getElementById('dog-div')
-// dogDiv.innerHTML += rex.getDogHtml()
+const appBody = document.querySelector('.app-body')
+const nopeButton = document.querySelector('#nope')
+const likeButton = document.querySelector('#like')
+
+const renderDogProfile = function(){
+    appBody.innerHTML = dogProfile.getDogHtml()
+}
+
+renderDogProfile()
+
+nopeButton.addEventListener("click", function(){
+    appBody.innerHTML += dogProfile.getNopeHtml()
+    disableButtons()
+})
+
+likeButton.addEventListener("click", function(){
+    appBody.innerHTML += dogProfile.getLikeHtml()
+    disableButtons()
+})
+
+const disableButtons = function(){
+    nopeButton.disabled = true
+    likeButton.disabled = true
+    console.log(dogs)
+    dogs.shift()
+    console.log(dogs)
+    appBody.innerHTML = ''
+    setTimeout(function (){
+        renderDogProfile()
+    }, 1000)
+}
